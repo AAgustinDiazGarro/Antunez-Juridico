060d1abbf092a9423c5ae3d87e548faea7f5aef6
unknown <agustin87_7@hotmail.com>
2023-09-05 11:07:25 -0300

    Agregando componente Abogados y Nosotros

---
+++ src/Componentes/Nosotros/Nosotros.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+
+
+const Nosotros = () => {
+
+
+    return(
+        <Fragment>
+            <h1>Aca podemos escribir mas detalladamente como es la historia del Estudio</h1>
+        </Fragment>
+    )
+}
+
+export default Nosotros;
